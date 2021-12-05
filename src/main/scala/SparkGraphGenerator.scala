a6492f7dce77874c90027be4d102164379d36134
Carlos Daniel Ríos Moya <cd.riosmoya@outlook.com>
2021-12-05 04:14:12 -0300

    Iterative implementation of generateEdge

--- src/main/scala/SparkGraphGenerator.scala
+++ src/main/scala/SparkGraphGenerator.scala
@@ -1,5 +1,7 @@
 import org.apache.spark.sql.SparkSession
 
+import scala.util.Random
+
 object SparkGraphGenerator {
   def main(args: Array[String]): Unit = {
     if (args.length != 2) {
@@ -15,7 +17,7 @@ object SparkGraphGenerator {
     println(s"Creating ${if (directed) "" else "un"}directed graph with $n nodes and $e edges")
 
     val distData = spark.sparkContext.range(0, e)
-    val pairs = distData.map(x => (new RMat().generateEdge(n)._1, 1))
+    val pairs = distData.map(x => (generateEdge(n)._1, 1))
     val degrees = pairs.reduceByKey(_ + _)
     val edges = if (directed) degrees.flatMap(pair => createDirectedEdges(pair._1, pair._2)) else degrees.flatMap(pair => createUndirectedEdges(pair._1, pair._2, n))
 
@@ -58,5 +60,43 @@ object SparkGraphGenerator {
       target += 1
     }
     return edges
+  }
+
+  def generateEdge(n: Long): (Long, Long) = {
+    var (a, b, c, d) = (0.75, 0.05, 0.19, 0.01)
+    val m = 0.25
+    val depth = math.ceil(math.log(n) / math.log(2))
+    val (offset1, offset2, offset3, offset4) = ((m - a) / depth, (m - b) / depth, (m - c) / depth, (m - d) / depth)
+    var (x1, y1, xn, yn) = (1.toLong, 1.toLong, n, n)
+
+    while (xn - x1 > 1 || yn - y1 > 1) {
+      val r = Random.nextDouble()
+      if (r < a) {
+        xn = math.floor((x1 + xn) / 2).toLong
+        yn = math.floor((y1 + yn) / 2).toLong
+      } else if (r < a + b) {
+        x1 = math.ceil((x1 + xn) / 2).toLong
+        yn = math.floor((y1 + yn) / 2).toLong
+      } else if (r < a + b + c) {
+        y1 = math.ceil((y1 + yn) / 2).toLong
+        xn = math.floor((x1 + xn) / 2).toLong
+      } else {
+        x1 = math.ceil((x1 + xn) / 2).toLong
+        y1 = math.ceil((y1 + yn) / 2).toLong
+      }
+
+      a = math.abs(a + offset1)
+      b = math.abs(b + offset2)
+      c = math.abs(c + offset3)
+      d = math.abs(d + offset4)
+    }
+
+    val r = Random.nextDouble()
+    if (xn - x1 == 0 && yn - y1 == 1)
+      return if (r < 0.5) (x1, y1) else (x1, yn)
+    else if (xn - x1 == 1 && yn - y1 == 0)
+      return if (r < 0.5) (x1, y1) else (xn, yn)
+    else
+      return (x1, y1)
   }
 }
